c99e23256c815d1b2f1ea6d8659fba890c7a6ed3
Buu <buddingnewinsights@gmail.com>
2026-05-13 20:38:56 +0700

    style: biome format setup.ts

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -9,10 +9,18 @@ Object.defineProperty(globalThis, 'localStorage', {
   configurable: true,
   value: {
     getItem: (k: string) => store[k] ?? null,
-    setItem: (k: string, v: string) => { store[k] = v },
-    removeItem: (k: string) => { delete store[k] },
-    clear: () => { for (const k of Object.keys(store)) delete store[k] },
-    get length() { return Object.keys(store).length },
+    setItem: (k: string, v: string) => {
+      store[k] = v
+    },
+    removeItem: (k: string) => {
+      delete store[k]
+    },
+    clear: () => {
+      for (const k of Object.keys(store)) delete store[k]
+    },
+    get length() {
+      return Object.keys(store).length
+    },
     key: (i: number) => Object.keys(store)[i] ?? null,
   } satisfies Storage,
 })
